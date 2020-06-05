d3340fcb02da0d4e34fe707db02bd4b19cb3f639
jigi <jigi@jigi.net>
2020-06-05 21:52:56 +0900

    5장 실습 - 네이버 로그인 완성

--- src/main/java/com/jigi/study/springboot/Application.java
+++ src/main/java/com/jigi/study/springboot/Application.java
@@ -2,9 +2,8 @@ package com.jigi.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA AUDITION 활성화
+//@EnableJpaAuditing // JPA AUDITION 비활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {

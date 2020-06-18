66c3418c974b3088993de890a3f6bb60337f9c39
jigi <jigi@jigi.net>
2020-06-18 23:03:24 +0900

    10장 실습 - 무중단 배포를 위한 배치스크립트 오자 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-ABSPATH=${readlink -f $0}
-ABSDIR=${dirname $ABSPATH}
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 function switch_proxy() {
